8206476b642ef951e5bd0b693a23524c3ca9f844
Fu <xufu2022@gmail.com>
2022-05-05 07:05:13 +0100

    started end

--- Module2.BeforeDI/Module2.BeforeDI/Program.cs
+++ Module2.BeforeDI/Module2.BeforeDI/Program.cs
@@ -6,21 +6,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
+    .ConfigureServices((_, services) =>
     {
         services.AddTransient<Configuration>();
+
+        services.AddTransient<IPriceParser, PriceParser>();
+        services.AddTransient<IProductSource, ProductSource>();
+
+        services.AddTransient<IProductFormatter, ProductFormatter>();
+        services.AddTransient<IProductTarget, ProductTarget>();
+
+        services.AddTransient<ProductImporter>();
     })
     .Build();
 
 var productImporter = host.Services.GetRequiredService<ProductImporter>();
+
 productImporter.Run();
-
-
-var configuration = new Configuration();
-
-var priceParser = new PriceParser();
-var productSource = new ProductSource(configuration, priceParser);
-
-var productFormatter = new ProductFormatter();
-var productTarget = new ProductTarget(configuration, productFormatter);
-
--- Module2.BeforeDI/Module2.BeforeDI/Shared/Configuration.cs
+++ Module2.BeforeDI/Module2.BeforeDI/Shared/Configuration.cs
@@ -4,6 +4,6 @@ public class Configuration
     // We will deal with passing in configuration in a better way in a future module
     // For now hardcoding the values is enough to practice with the concepts from this module
 
-    public string SourceCsvPath => @"c:\src-pluralsight\product-input.csv";
-    public string TargetCsvPath => @"c:\src-pluralsight\product-output.csv";
+    public string SourceCsvPath => @"E:\dev\2022\05\pluralsight\c10dependinject\pl-dependInject\Module2.BeforeDI\Module2.BeforeDI\product-input.csv";
+    public string TargetCsvPath => @"E:\dev\2022\05\pluralsight\c10dependinject\pl-dependInject\Module2.BeforeDI\Module2.BeforeDI\product-output.csv";
 }
